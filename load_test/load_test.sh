68a095f6b4bf15cfbf613b7281cc7a34173da37b
vladkuz54 <vkuz1540@gmail.com>
2025-10-17 15:05:13 +0300

    update load_test.sh

--- load_test/load_test.sh
+++ load_test/load_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ENDPOINT="http://13.60.169.201:5000/apidocs"
+ENDPOINT="http://13.60.16.200:5000/apidocs"
 
 echo "Start testing..."
-wrk -t8 -c500 -d5m $ENDPOINT/ >> ab_results.log
+wrk -t8 -c500 -d5m $ENDPOINT
 sleep 5 
